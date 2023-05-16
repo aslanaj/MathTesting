7832507599207a8276b9af318a753966cf4101ea
aslanaj <theajaslan@gmai.com>
2023-05-16 15:14:02 +0600

    добавил тест в main activity

--- app/src/main/java/com/example/mathtesting/MainActivity.kt
+++ app/src/main/java/com/example/mathtesting/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.mathtesting
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mathtesting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initClickers()
+    }
+
+    private fun initClickers() {
+        with(binding){
+            btnAdd.setOnClickListener {
+                tvResult.text = Math().add(etFirst.text.toString(),etSecond.text.toString())
+            }
+            btnDivide.setOnClickListener {
+                tvResult.text = Math().divide(etFirst.text.toString(),etSecond.text.toString())
+            }
+        }
     }
 }
