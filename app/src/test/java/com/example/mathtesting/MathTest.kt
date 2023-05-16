8b52ad623a41e9b3184b87d2a4ac4b6d220953bd
aslanaj <theajaslan@gmai.com>
2023-05-16 14:34:38 +0600

    Initial commit

---
+++ app/src/test/java/com/example/mathtesting/MathTest.kt
@@ -0,0 +1,57 @@
+package com.example.mathtesting
+
+import junit.framework.TestCase.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class MathTest {
+
+    var math: Math? = null
+
+    @Before
+    fun init() {
+        math = Math()
+    }
+
+    @Test
+    fun simpleAdd() {
+        assertEquals("12", math?.add("5", "7"))
+    }
+
+
+    @Test
+    fun symbolAdd() {
+        assertEquals("Нельзя вводить буквы", math?.add("5", "7dsakj"))
+    }
+
+    @Test
+    fun doubleAdd() {
+        assertEquals("12.0", math?.add("5.0", "7"))
+    }
+
+    @Test
+    fun simpleDivide() {
+        assertEquals("6", math?.divide("12", "2"))
+    }
+
+    @Test
+    fun zeroDivide() {
+        assertEquals("На ноль делить нельзя", math?.divide("12", "0"))
+    }
+
+    @Test
+    fun emptyAdd(){
+        assertEquals("Должно быть число",math?.divide("12",""))
+    }
+
+    @Test
+    fun negativeAdd(){
+        assertEquals("-12",math?.add("-5","-7"))
+    }
+
+    @After
+    fun detach() {
+        math = null
+    }
+}
